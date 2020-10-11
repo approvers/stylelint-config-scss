ae2025aae78a196c99b02dbe269516f23554dec6
shun-shobon <me@shun.technology>
2020-10-11 10:19:00 +0900

    feat: add invalid scss tests

--- tests/invalid.spec.js
+++ tests/invalid.spec.js
@@ -51,3 +51,51 @@ describe("invalid.css", () => {
     expect(result.results[0].warnings[0].column).toBe(8);
   });
 });
+
+describe("invalid.scss", () => {
+  const scss = fs.readFileSync(path.resolve(__dirname, "invalid.scss"), "utf-8");
+  const runLint = () =>
+    stylelint.lint({
+      code: scss,
+      config,
+    });
+
+  it("should return error", async () => {
+    const result = await runLint();
+    console.log(result);
+    expect(result.errored).toBeTruthy();
+  });
+
+  it("should return one warnings", async () => {
+    const result = await runLint();
+    console.log(JSON.stringify(JSON.parse(result.output), null, 2));
+    expect(result.results[0].warnings).toHaveLength(1);
+  });
+
+  it("warning text should be 'Unexpected () used to surround statements for @-rules (scss/at-rule-conditional-no-parentheses)'", async () => {
+    const result = await runLint();
+    expect(result.results[0].warnings[0].text).toBe(
+      "Unexpected () used to surround statements for @-rules (scss/at-rule-conditional-no-parentheses)",
+    );
+  });
+
+  it("warning rule should be 'scss/at-rule-conditional-no-parentheses'", async () => {
+    const result = await runLint();
+    expect(result.results[0].warnings[0].rule).toBe("scss/at-rule-conditional-no-parentheses");
+  });
+
+  it("warning severity should be 'error'", async () => {
+    const result = await runLint();
+    expect(result.results[0].warnings[0].severity).toBe("error");
+  });
+
+  it("warning line should be '1'", async () => {
+    const result = await runLint();
+    expect(result.results[0].warnings[0].line).toBe(1);
+  });
+
+  it("warning column should be '1'", async () => {
+    const result = await runLint();
+    expect(result.results[0].warnings[0].column).toBe(1);
+  });
+});
