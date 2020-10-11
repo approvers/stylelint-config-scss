6274df0bec4e6bcda6a5f06d5ee25197bf528998
shun-shobon <me@shun.technology>
2020-10-11 10:04:54 +0900

    test: add valid scss tests

--- tests/valid.spec.js
+++ tests/valid.spec.js
@@ -24,3 +24,22 @@ describe("valid.css", () => {
     expect(result.results[0].warnings).toHaveLength(0);
   });
 });
+
+describe("valid.scss", () => {
+  const scss = fs.readFileSync(path.resolve(__dirname, "valid.scss"), "utf-8");
+  const runLint = () =>
+    stylelint.lint({
+      code: scss,
+      config,
+    });
+
+  it("should return no errors", async () => {
+    const result = await runLint();
+    expect(result.errored).toBeFalsy();
+  });
+
+  it("should return no warnings", async () => {
+    const result = await runLint();
+    expect(result.results[0].warnings).toHaveLength(0);
+  });
+});
